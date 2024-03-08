e8a11b0cd2edb976d4c95bb5e7dbdda97b19c066
hlomambalula88@gmail.com <hlomambalula88@gmail.com>
2024-03-08 20:30:45 +0200

    base of wesite

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDropdown(){
+    const dropdown = document.querySelector("#dropdown")
+    dropdown.style.display="flex"
+}
+function hideDropdown(){
+    const sidebar = document.querySelector("#dropdown")
+    dropdown.style.display="none"
+}
